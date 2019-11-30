69da3502d83cf96cc9a2e31f023529795ae09350
Simon Smale <simon@smalemail.net>
2019-11-30 12:25:07 +0000

    init commit

---
+++ GildedRoseApp.Console.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using ConsoleApplication;
+using Xunit;
+
+namespace GildedRoseApp.Console.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
