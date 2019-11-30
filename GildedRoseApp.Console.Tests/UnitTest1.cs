c6f933ce6b45742e00e976b3ab8edc3a9ad15426
Simon Smale <simon@smalemail.net>
2019-11-30 18:46:00 +0000

    adds test for passes between 10 and 6 days

--- GildedRoseApp.Console.Tests/UnitTest1.cs
+++ GildedRoseApp.Console.Tests/UnitTest1.cs
@@ -79,5 +79,21 @@ namespace GildedRoseApp.Console.Tests
 
             Assert.Equal(11, app.GetItems()[0].Quality);
         }
+
+        [Fact]
+        public void BackstagePasses_LessThan10DaysButGreaterThanFive_QualityIncreasesByTwo()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10 },
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 6, Quality = 10 }
+            };
+            var app = new Program(Items);
+            app.UpdateQuality();
+
+            // 10 days
+            Assert.Equal(12, app.GetItems()[0].Quality);
+            // 6 days
+            Assert.Equal(12, app.GetItems()[1].Quality);
+        }
     }
 }
