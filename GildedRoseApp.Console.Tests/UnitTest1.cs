9bd12a58986a004177af0d4831a6f7e14bad2ec5
Simon Smale <simon@smalemail.net>
2019-11-30 18:03:40 +0000

    adds test for a normal itrem after 1 day
    also removes the default test
    renames the ctor/get test to something more meaningful

--- GildedRoseApp.Console.Tests/UnitTest1.cs
+++ GildedRoseApp.Console.Tests/UnitTest1.cs
@@ -9,16 +9,24 @@ namespace GildedRoseApp.Console.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
-            Assert.True(true);
-        }
-        [Fact]
-        public void Test2()
+        public void CanSetandReadItems()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             var app = new Program(Items);
             Assert.Equal("foo", app.GetItems()[0].Name);
+        }
+
+        [Fact]
+        public void NormalItemAfterOneDay_QualityandSellInDecrementedByOne()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            var app = new Program(Items);
+            app.UpdateQuality();
+
+            // Quality should be 9
+            Assert.Equal(9, app.GetItems()[0].Quality);
+            // SellIn should be 9
+            Assert.Equal(9, app.GetItems()[0].SellIn);
         }
     }
 }
